26f1c17c67b4f7fedf0516c3429212c49989bd53
Jackson Vargas <jacksonvargas134@gmail.com>
2023-10-27 20:24:02 -0300

    Mudanças no Layout

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -15,37 +15,10 @@ export default function Header() {
         <div className={`flex justify-between items-center`}>
           <Link className={`text-2xl text-zinc-200 font-semibold`} href='/'>Port<span className={`text-blue-600`}>fólio</span></Link>
 
-          <ul className={`flex items-center max-md:hidden`}>
-            <Link className={`ml-5 hover:text-blue-600 link ${pathname === '/' ? 'text-blue-600' : 'text-zinc-200'}`} href='/'>Inicío</Link>
-            <Link className={`ml-5 hover:text-blue-600 link ${pathname === '/sobre' ? 'text-blue-600' : 'text-zinc-200'}`} href='/sobre'>Sobre</Link>
-            <Link className={`ml-5 hover:text-blue-600 link ${pathname === '/habilidades' ? 'text-blue-600' : 'text-zinc-200'}`} href='/habilidades'>Habilidades</Link>
-            <Link className={`ml-5 hover:text-blue-600 link ${pathname === '/projetos' ? 'text-blue-600' : 'text-zinc-200'}`} href='/projetos'>Projetos</Link>
-            <Link className={`ml-5 hover:text-blue-600 link ${pathname === '/contato' ? 'text-blue-600' : 'text-zinc-200'}`} href='/contato'>Contato</Link>
-          </ul>
-
-          <div className={`flex items-center max-md:hidden`}>
+          <div className={`flex items-center`}>
             <Link className={`text-xl text-zinc-200 hover:text-blue-600`} href='https://github.com/jacksonVargas' target='_blank'><FaGithub /></Link>
             <Link className={`text-xl ml-5 text-zinc-200 hover:text-blue-600`} href='https://www.linkedin.com/in/jackson-vargas-29a486287/' target='_blank'><FaLinkedin /></Link>
           </div>
-
-        { /*MOBILE*/ }
-          <ul className={`${menu ? 'max-w-[280px]' : 'hidden'} w-full flex flex-col md:hidden fixed top-0 left-0 h-screen bg-black border-r border-zinc-200`}>
-            <Link className={`m-5 ml-3 hover:text-blue-600 link ${pathname === '/' ? 'text-blue-600' : 'text-zinc-200'}`} href='/'>Inicío</Link>
-            <Link className={`mb-5 ml-3 hover:text-blue-600 link ${pathname === '/sobre' ? 'text-blue-600' : 'text-zinc-200'}`} href='/sobre'>Sobre</Link>
-            <Link className={`mb-5 ml-3 hover:text-blue-600 link ${pathname === '/habilidades' ? 'text-blue-600' : 'text-zinc-200'}`} href='/habilidades'>Habilidades</Link>
-            <Link className={`mb-5 ml-3 hover:text-blue-600 link ${pathname === '/projetos' ? 'text-blue-600' : 'text-zinc-200'}`} href='/projetos'>Projetos</Link>
-            <Link className={`mb-5 ml-3 hover:text-blue-600 link ${pathname === '/contato' ? 'text-blue-600' : 'text-zinc-200'}`} href='/contato'>Contato</Link>
-          
-            <div className={`flex items-center absolute bottom-20 left-5`}>
-              <Link className={`text-xl text-zinc-200 hover:text-blue-600`} href='https://github.com/jacksonVargas' target='_blank'><FaGithub /></Link>
-              <Link className={`text-xl ml-5 text-zinc-200 hover:text-blue-600`} href='https://www.linkedin.com/in/jackson-vargas-29a486287/' target='_blank'><FaLinkedin /></Link>
-            </div>
-          </ul>
-
-          <button className={`md:hidden text-2xl text-zinc-200 hover:text-blue-600`} onClick={() => setMenu(!menu)}>
-            {menu ? <FaTimes /> : <FaBars />}
-          </button>
-        { /*MOBILE*/ }
         </div>
       </header>
     </>
